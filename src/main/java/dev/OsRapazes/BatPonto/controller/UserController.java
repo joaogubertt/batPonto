57ae103382c6b2fa984753de0fb401e7a74d5676
joao.gubertt <joaogubertt@gmail.com>
2025-12-24 12:00:46 -0300

    feature: creates User Controller and bypass jwt validation till isnt implementated

---
+++ src/main/java/dev/OsRapazes/BatPonto/controller/UserController.java
@@ -0,0 +1,32 @@
+package dev.OsRapazes.BatPonto.controller;
+
+import dev.OsRapazes.BatPonto.dto.User.RegisterUserDto;
+import dev.OsRapazes.BatPonto.dto.User.UserResponseDto;
+import dev.OsRapazes.BatPonto.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> create(@RequestBody @Valid RegisterUserDto data, UriComponentsBuilder uriBuilder){
+        UserResponseDto user = userService.registerUser((data));
+
+        URI uri = uriBuilder.path("/users/{id}").buildAndExpand(user.id()).toUri();
+
+        return ResponseEntity.created(uri).body(user);
+    }
+}
