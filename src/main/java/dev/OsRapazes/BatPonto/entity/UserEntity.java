d942127717a2dc3b1b99bdbaecd2b1c8b6ffca47
joao.gubertt <joaogubertt@gmail.com>
2025-12-24 15:45:42 -0300

    feature: implements UserDetail do validate with Spring Security, creates AuthorizationService and TokenService to start JWT login verification, add jwt dependency in pom.xml and increases app.properties with jwt expiration time and temporary generic secrets
    # Conflicts:
    #	pom.xml

--- src/main/java/dev/OsRapazes/BatPonto/entity/UserEntity.java
+++ src/main/java/dev/OsRapazes/BatPonto/entity/UserEntity.java
@@ -6,8 +6,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -15,7 +20,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserEntity {
+public class UserEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -39,4 +44,36 @@ public class UserEntity {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private Instant updatedAt;
+
+    @Override
+    public String getPassword(){
+        return passwordHash;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if ("RH".equalsIgnoreCase(this.role)) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+        else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getUsername(){
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {return true; }
+
+    @Override
+    public boolean isAccountNonLocked() {return true; }
+
+    @Override
+    public boolean isCredentialsNonExpired() {return true; }
+
+    @Override
+    public boolean isEnabled() {return true; }
 }
