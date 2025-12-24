06554897b609f01d65f785724d4a3f0d06c14fe2
joao.gubertt <joaogubertt@gmail.com>
2025-12-24 00:51:43 -0300

    feat: creates user entity

--- src/main/java/dev/OsRapazes/BatPonto/entity/UserEntity.java
+++ src/main/java/dev/OsRapazes/BatPonto/entity/UserEntity.java
@@ -1,19 +1,38 @@
 package dev.OsRapazes.BatPonto.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
+@Table(name = "tb_users")
 @Data
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false)
     private String name;
-    private String password;
+
+    @Column(nullable = false)
     private String passwordHash;
-    private String role;
+
+    @Column(nullable = false)
+    private String role; // -> Transformar em ENUM
+
+    @CreationTimestamp
+    @Column(updatable = false)
     private Instant createdAt;
+
+    @UpdateTimestamp
     private Instant updatedAt;
 }
